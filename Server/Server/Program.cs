c1651ed0ce1e9e5fc43f8f5e25f51e0545d70c6d
Filip <filipwiii@gmail.com>
2020-09-20 09:39:37 +0200

    #1 First Commit

---
+++ Server/Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "null - Server";
+            Server.Start(50, 26950);
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
